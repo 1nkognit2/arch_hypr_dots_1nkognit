61d43797afbb801ec4d9de91933805c98b514f2d
1nkognit2 <alhimet09k@gmail.com>
2025-04-26 15:41:05 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,28 @@
 script_dir=$(dirname "$(realpath "$0")")
 
-echo "     INSTALL SCRIPT"
+sudo -v
+
+(
+    while true; do
+        sleep 600
+        sudo -v
+    done
+) &
+PID=$!
+
 sleep 1
 
-echo "    Installing paru"
 cd ~
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+git clone --depth 1 https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+makepkg -si --noconfirm
 cd ~
-sudo rm -rf paru
-echo "    Paru installed"
+sudo rm -rf paru-bin
 sleep 1
-echo ""
 
-echo "    Installing python"
 sudo pacman -S --noconfirm python
-echo "    Python installed"
 sleep 1
-echo ""
 
-echo "    Installing Packages"
 python "$script_dir/install_packages.py"
-echo "    Packages installed"
-sleep 1
-echo ""
-
-echo "    Installing configs"
 python "$script_dir/install_homefiles.py"
-echo "    Configs installed"
-sleep 1
-echo ""
-
-echo "    Post-installing"
 python "$script_dir/post_install.py"
