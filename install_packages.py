49ea997b1e7551734a1f28c2edf22af994f59aa6
vova_4104 <v.4104@yandex.com>
2025-04-26 19:15:36 +0300

    Hotfix

--- install_packages.py
+++ install_packages.py
@@ -54,7 +54,7 @@ packages = {
     ],
 
     'Aur': [
-        'hyprshot-bin',
+        'hyprshot',
         'oh-my-posh-bin',
         'pinta',
         'nwg-look',
@@ -84,5 +84,4 @@ pacman_parsed = ' '.join(packages['Pacman'])
 aur_parsed = ' '.join(packages['Aur'])
 
 os.system(f'sudo pacman -S --noconfirm --needed {pacman_parsed}')
-os.system(f'paru -Sy')
-os.system(f'paru -S --noconfirm {aur_parsed}')
+os.system(f'paru -Sy && paru -S --noconfirm {aur_parsed}')
