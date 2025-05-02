4d911e42797fd1d3197f3052c2b7f0193654922a
vova_4104 <4104vova@gmail.com>
2025-05-02 12:18:05 +0300

    Split install files

---
+++ modules/install_homefiles.py
@@ -0,0 +1,24 @@
+import pathlib, shutil, os
+
+def install_homefiles():
+	file_dir = pathlib.Path(__file__).parent.resolve()
+	home = pathlib.Path(os.path.expanduser('~')).resolve()
+	source_dir = file_dir / 'home'
+
+	def copy_with_replace(src, dst):
+		try:
+			if src.is_dir():
+				if not dst.exists():
+					dst.mkdir(parents=True)
+				for item in src.iterdir():
+					copy_with_replace(item, dst / item.name)
+			else:
+				shutil.copy2(src, dst, follow_symlinks=False)
+		except Exception as e:
+			pass
+
+	if source_dir.exists():
+		for item in source_dir.iterdir():
+			copy_with_replace(item, home / item.name)
+		
+		shutil.rmtree(source_dir, ignore_errors=True)
